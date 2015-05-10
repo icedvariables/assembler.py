d0397b5e3a219360017ab0883b5f29af9f4674f3
icedvariables <macodeblacko@gmail.com>
2015-05-10 20:24:59 +0100

    Added all instructions

--- assembler.py
+++ assembler.py
@@ -1,31 +1,30 @@
 import re
 
-class Instructions:
-    PUSH,POP,SET,ADD,SUB,MUL,DIV,JUMP,HALT,PRINT,JUMPIFEQUAL,JUMPIFNOTEQUAL,JUMPIFGT,JUMPIFLT = range(1, 15)
-
 class Assembler:
     def __init__(self):
-        self.RE_PUSH = re.compile(r"push\W+(\d+)")
-        self.RE_POP = re.compile(r"pop")
+        self.instructions = {"PUSH":0x1,"POP":0x2,"SET":0x3,"ADD":0x4,"SUB":0x5,"MUL":0x6,"DIV":0x7,"JUMP":0x8,"HALT":0x9,
+                             "PRINT":0xA,"JUMPIFEQUAL":0xB,"JUMPIFNOTEQUAL":0xC,"JUMPIFGT":0xD,"JUMPIFLT":0xE}
 
         self.out = []
 
     def assemble(self, code):
         for line in code.split("\n"):
-            line = line.strip()
+            line = line.strip().replace(" " * 2, " ").upper()
+            split = line.split(" ")
 
-            # PUSH
-            match = self.RE_PUSH.match(line)
-            if(match):
-                value = int(match.group(1))
+            if not(line):
+                continue
 
-                self.out.append(Instructions.PUSH)
-                self.out.append(value)
+            if(len(split) > 0):
+                if(split[0] in self.instructions.keys()):
+                    instruction = self.instructions[split[0]]
 
-            # POP
-            match = self.RE_POP.match(line)
-            if(match):
-                self.out.append(Instructions.POP)
+                    self.out.append(instruction) # Add instruction
+                    self.out.extend(split[1:])   # Add arguments
+                else:
+                    print "Unknown instruction:", split[0]
+
+
 
         return self.out
 
